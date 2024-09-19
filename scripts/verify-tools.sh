7df6af5eb59f489cbd6f3e249d73be657b3356cd
TiGz <ajchesney@gmail.com>
2024-09-19 12:11:51 +0100

    Update nvm installation URL to v0.40.1

--- scripts/verify-tools.sh
+++ scripts/verify-tools.sh
@@ -7,7 +7,7 @@ echo "Verifying and installing required tools..."
 # Function to install nvm
 install_nvm() {
     echo "Installing nvm..."
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 }
